2f0ea901bb516f022e4e836e1ea88b3e496e4048
snoopyxpl <chaigneaunicolas25@gmail.com>
2023-03-31 17:19:47 +0200

    jsooooonnnnnnnnnnnn

--- src/main/java/Character/EnumEnemy.java
+++ src/main/java/Character/EnumEnemy.java
@@ -1,6 +1,33 @@
 package main.java.Character;
 
+import java.util.List;
+import java.util.ArrayList;
 public enum EnumEnemy {
+    TROLL(100, 0.1, 0.1, 0.1, 0.1, 0, 0, 0, 0, "Troll", null),
     ;
 
+    private int lifepoint;
+    private double damagereseitancebonus;
+    private double damageresitance;
+    private double defance;
+    private double damagebonus;
+    private int spelldamage;
+    private int potionbonus;
+    private int precision;
+    private String enemyname;
+    private List<Spell> usablespell;
+
+    EnumEnemy(int lifepoint, double damagereseitancebonus, double damageresitance, double defance,
+            double damagebonus, int spelldamage, int precision, int precisionbonus,
+            double damage, String enemyname, List<Spell> usablespell) {
+        this.lifepoint = lifepoint;
+        this.damagereseitancebonus = damagereseitancebonus;
+        this.damageresitance = damageresitance;
+        this.defance = defance;
+        this.damagebonus = damagebonus;
+        this.spelldamage = spelldamage;
+        this.usablespell = new ArrayList<>();
+        this.precision = precision;
+
+    }
 }
--- src/main/java/Console/App.java
+++ src/main/java/Console/App.java
@@ -1,6 +1,9 @@
 package main.java.Console;
+
+import main.java.Character.Wizzard;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
     }
 }
