87b24829dbff4ea12016def2738ea5f81e4893da
snoopyxpl <chaigneaunicolas25@gmail.com>
2023-04-01 11:14:30 +0200

    rebuild

--- bin/main/java/Character/AbsSpell.java
+++
@@ -1,7 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package main.java.Character;
-
-public abstract class AbsSpell {
-}
--- bin/main/java/Console/Game.java
+++
@@ -1,7 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package main.java.Console;
-
-public class Game {
-}
