3272b4bbc25eff5db556866d3ce2ae6e0f42d213
snoopyxpl <chaigneaunicolas25@gmail.com>
2023-04-20 19:01:49 +0200

    fix attack pour tout les enemy

--- src/main/java/Game/enemys/Basilic.java
+++ src/main/java/Game/enemys/Basilic.java
@@ -1,13 +1,3 @@
 package main.java.Game.enemys;
 
-import main.java.Game.spell.Spell;
-import main.java.Game.wizzard.Wizzard;
-
-public class Basilic extends AbsEnemy {
-
-  @Override
-  public void attack(Wizzard wizzard, Spell spell, EnumEnemy enemy) {
-    // TODO Auto-generated method stub
-
-  }
-}
+public class Basilic extends Enemy {}
--- src/main/java/Game/enemys/Troll.java
+++ src/main/java/Game/enemys/Troll.java
@@ -1,10 +1,3 @@
 package main.java.Game.enemys;
 
-import main.java.Game.spell.Spell;
-import main.java.Game.wizzard.Wizzard;
-
-public class Troll extends AbsEnemy {
-
-  @Override
-  public void attack(Wizzard wizard, Spell spell, EnumEnemy enemy) {}
-}
+public class Troll extends Enemy {}
