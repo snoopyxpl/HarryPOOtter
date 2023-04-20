a623d664d2126abb7d3a4cc4b1812e4e0acf5f7e
snoopyxpl <chaigneaunicolas25@gmail.com>
2023-04-20 19:49:41 +0200

    Levels!!!!!!!!!!!!!!!!

---
+++ src/main/java/Game/levels/ChambreDesSecrets.java
@@ -0,0 +1,8 @@
+package main.java.Game.levels;
+
+public class ChambreDesSecrets extends Levels {
+
+  public ChambreDesSecrets() {
+    System.out.println("ChambreDesSecrets");
+  }
+}
---
+++ src/main/java/Game/levels/CimetièreDeLittleHangleton.java
@@ -0,0 +1,8 @@
+package main.java.Game.levels;
+
+public class CimetièreDeLittleHangleton extends Levels {
+
+  public CimetièreDeLittleHangleton() {
+    System.out.println("CimetièreDeLittleHangleton");
+  }
+}
