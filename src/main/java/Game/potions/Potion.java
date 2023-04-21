ab3ff34169ffbf0d4e8570325a20b44956d38d6a
snoopyxpl <chaigneaunicolas25@gmail.com>
2023-04-21 08:28:24 +0200

    abspotion

--- src/main/java/Game/potions/Potion.java
+++ src/main/java/Game/potions/Potion.java
@@ -3,7 +3,7 @@ package main.java.Game.potions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Potion {
+public class Potion extends AbsPotion {
 
   private final EnumPotion potionType;
 
